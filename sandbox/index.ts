fe02e640d835bccab3087799e009b72d236748ec
Jason Joannou <jjoannou@bscglobal.com>
2024-08-26 23:20:42 +0200

    Added post method

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,10 @@
 import express, {Request, Response} from 'express';
 
+// Interfaces
+interface GreetQuery {
+    name: string;
+}
+
 // Create express application
 const app = express();
 const port = 3000;
@@ -21,9 +26,16 @@ app.get('/greet_url/:name', (req: Request, res: Response) => {
 
 //Query Parameter
 // http://localhost:3000/greet_query?name=Jason
-app.get('/greet_query', (req: Request, res: Response) => {
+app.get('/greet_query', (req: Request<{}, {}, {}, GreetQuery>, res: Response) => {
     const name = req.query.name as string; // we are accessing the name request from the url
     res.send(`Hello, ${name}!`);
+});
+
+// POST Request
+// simple pay
+app.post('/pay', (req: Request, res: Response) => {
+    const amount = req.body.amount; // we are accessing the amount request from the body
+    res.send(`Pay ${amount}!`);
 });
 
 // Start server
