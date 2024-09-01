ef14bd23638adc0c64996b2cddab35a2ca7183e7
Jason Joannou <jjoannou@bscglobal.com>
2024-09-01 11:33:10 +0200

    Steps for choice selection from user

--- whatsapp-chatbot/src/message_config.py
+++ whatsapp-chatbot/src/message_config.py
@@ -1,3 +1,9 @@
 JOIN_STOCKVEL = {
-    
+    "step_1": {
+        "message": "Please select the stockvel you want to join:",
+    },
+    "step_2": {
+        "message": "Please provide the amount you want to contribute towards the stockvel:",
+    }
+
 }
