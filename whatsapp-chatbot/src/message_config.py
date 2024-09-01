ff2176b7ced7c3e58a3c11827a375ed73f066c5d
Jason Joannou <jjoannou@bscglobal.com>
2024-09-01 20:13:37 +0200

    Added more message configs

--- whatsapp-chatbot/src/message_config.py
+++ whatsapp-chatbot/src/message_config.py
@@ -1,3 +1,7 @@
+REGISTER_NUMBER = {
+    "message": "Please provide your phone number in the format `+1XXXXXXXXXX`:"
+}
+
 JOIN_STOCKVEL = {
     "step_1": {
         "message": "Please select the stockvel you want to join:",
@@ -7,3 +11,30 @@ JOIN_STOCKVEL = {
     }
 
 }
+
+GREET_MESSAGE_REGISTERED = {
+    "message": """Hi there! Please select one of the following options to proceed: 
+    1. Join a stockvel 
+    2. Leave a stockvel
+    3. Contribute to a stockvel
+    4. Payout from a stockvel
+    5. More Services"""
+}
+
+GREET_MESSAGE_UNREGISTERED = {
+    "message": """Hi there! We noticed this number is not registered with us. Registering your number is needed to start contributing to Stokvels.
+    
+    Please select one of the following options to proceed: 
+    1. Register using Rafiki money
+    2. Register using MTN Mobile Money"""
+}
+
+MORE_SERVICES = {
+    "message" : """
+    Please select one of the following options to proceed:
+    
+    1. View all available stockvels
+    2. Pay Reciepient"""
+}
+
+# 25291716
