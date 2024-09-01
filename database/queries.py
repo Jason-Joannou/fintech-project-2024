68135c9a496054baa469c7f304ca8cb65a4f24e2
Jason Joannou <jjoannou@bscglobal.com>
2024-09-01 20:28:43 +0200

    Introduced queries file

---
+++ database/queries.py
@@ -0,0 +1,27 @@
+from .sqlite_connection import SQLiteConnection
+from .sql_connection import sql_connection
+
+sqlite_conn = SQLiteConnection(database="./test_db.db")
+sql_conn = sql_connection()
+
+def check_if_number_exists_sqlite(from_number):
+
+    query = f"SELECT * FROM numbers WHERE number = :from_number"
+    with sqlite_conn.connect() as conn:
+        cursor = conn.execute(query, {'from_number': from_number})
+        result = cursor.fetchone()
+        if result:
+            return True
+        else:
+            return False
+        
+def check_if_number_exists_sql(from_number):
+
+    query = f"SELECT * FROM numbers WHERE number = :from_number"
+    with sql_conn.connect() as conn:
+        cursor = conn.execute(query, {'from_number': from_number})
+        result = cursor.fetchone()
+        if result:
+            return True
+        else:
+            return False
