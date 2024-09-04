3c2735fcc29fbf3cf3b60330d8f2158b8dd24f1e
Jason Joannou <jjoannou@bscglobal.com>
2024-09-04 10:26:43 +0200

    Initialized testing module

---
+++ whatsapp-chatbot/tests/test_action_calls.py
@@ -0,0 +1,7 @@
+
+
+def test_greeting_action_call(from_number: str, action_message: str):
+    raise NotImplementedError("Implement Standard Greeting response")
+
+def test_join_stokvel_action(from_number: str, action_message: str):
+    raise NotImplementedError("Implement actions for joining stokvel")
