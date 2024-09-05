64a1a722f97ab724b3d791de94fa71ecc6e86e86
Jason Joannou <jjoannou@bscglobal.com>
2024-09-05 09:46:31 +0200

    Removed SQL server implementation till we have a server running

--- database/create_tables.py
+++ database/create_tables.py
@@ -1,18 +1,32 @@
 from .sql_connection import sql_connection
 from .sqlite_connection import SQLiteConnection
+from sqlalchemy import text
 
-sqlite_conn = SQLiteConnection(database="./test_db.db")
-sql_conn = sql_connection()
+sqlite_conn = SQLiteConnection(database="./database/test_db.db")
+# sql_conn = sql_connection()
 
 # URL refers to the openAPI address of the stokvel
 
+def create_stokvel_members_table_sqlite():
+    with sqlite_conn.connect() as conn:
+        conn.execute(text("""
+            CREATE TABLE IF NOT EXISTS STOKVEL_MEMBERS (
+                id INTEGER PRIMARY KEY, 
+                stokvel_id INTEGER,
+                stokvel_name TEXT,
+                user_id INTEGER,
+                created_at TIMESTAMP, 
+                updated_at TIMESTAMP
+            )
+        """))
+
 def create_stokvel_table_sqlite():
     with sqlite_conn.connect() as conn:
-        conn.execute("""
+        conn.execute(text("""
             CREATE TABLE IF NOT EXISTS STOKVELS (
                 id INTEGER PRIMARY KEY, 
                 name TEXT UNIQUE,  -- Add UNIQUE constraint here
-                url TEXT, 
+                stokvel_wallet TEXT, 
                 stokvel_type TEXT,
                 min_contributing_amount INTEGER,
                 max_number_of_contributors INTEGER,
@@ -20,76 +34,39 @@ def create_stokvel_table_sqlite():
                 created_at TIMESTAMP, 
                 updated_at TIMESTAMP
             );
-        """)
+        """))
 
-def create_stokvel_table_sql_server():
-    with sql_conn.connect() as conn:
-        conn.execute("""
-            CREATE TABLE IF NOT EXISTS STOKVELS (
-                id INTEGER PRIMARY KEY, 
-                name TEXT UNIQUE,  -- Add UNIQUE constraint here
-                url TEXT,
-                stokvel_type TEXT,
-                min_contributing_amount INTEGER,
-                max_number_of_contributors INTEGER,
-                payout_day INTEGER,
-                created_at DATETIME, 
-                updated_at DATETIME
-            );
-        """)
 
 def create_user_table_sqlite():
     with sqlite_conn.connect() as conn:
-        conn.execute("""
+        conn.execute(text("""
             CREATE TABLE IF NOT EXISTS USERS (
-                id INTEGER PRIMARY KEY, 
-                name TEXT, 
+                user_id INTEGER PRIMARY KEY, 
+                user_number TEXT,
+                wallet_address TEXT,
                 created_at TIMESTAMP, 
                 updated_at TIMESTAMP
             );
-        """)
-
-def create_user_table_sql_server():
-    with sql_conn.connect() as conn:
-        conn.execute("""
-            CREATE TABLE IF NOT EXISTS USERS (
-                id INTEGER PRIMARY KEY, 
-                name TEXT, 
-                created_at DATETIME, 
-                updated_at DATETIME
-            )
-    """)
+        """))
 
 def create_transaction_table_sqlite():
     with sqlite_conn.connect() as conn:
-        conn.execute("""
-                     CREATE TABLE IF NOT EXISTS TRANSACTIONS (
-            id INTEGER PRIMARY KEY, 
-            user_id INTEGER, 
-            stokvel_id INTEGER, 
-            amount INTEGER, 
-            created_at TIMESTAMP, 
-            updated_at TIMESTAMP
+        conn.execute(text("""
+            CREATE TABLE IF NOT EXISTS TRANSACTIONS (
+                id INTEGER PRIMARY KEY, 
+                user_id INTEGER, 
+                stokvel_id INTEGER,
+                stokvel_name TEXT,
+                stokvel_type TEXT,
+                amount INTEGER, 
+                created_at TIMESTAMP, 
+                updated_at TIMESTAMP
         );
-    """)
-
-def create_transaction_table_sql_server():
-
-    with sql_conn.connect() as conn:
-        conn.execute("""
-        CREATE TABLE IF NOT EXISTS TRANSACTIONS (
-            id INTEGER PRIMARY KEY, 
-            user_id INTEGER, 
-            stokvel_id INTEGER, 
-            amount INTEGER, 
-            created_at DATETIME, 
-            updated_at DATETIME
-        );
-    """)
+    """))
 
 def create_resource_table_sqlite():
     with sqlite_conn.connect() as conn:
-        conn.execute("""
+        conn.execute(text("""
             CREATE TABLE IF NOT EXISTS RESOURCES (
                 id INTEGER PRIMARY KEY, 
                 name TEXT, 
@@ -97,28 +74,7 @@ def create_resource_table_sqlite():
                 created_at TIMESTAMP, 
                 updated_at TIMESTAMP
             );
-        """)
+        """))
 
-def create_resource_table_sql_server():
-    engine = sql_conn.get_engine()
-    engine.execute(
-        """
-        CREATE TABLE IF NOT EXISTS RESOURCES (
-            id INTEGER PRIMARY KEY, 
-            name TEXT, 
-            url TEXT, 
-            created_at DATETIME, 
-            updated_at DATETIME
-        );
-        """
-    )
-    with sql_conn.connect() as conn:
-        conn.execute("""
-            CREATE TABLE IF NOT EXISTS RESOURCES (
-                id INTEGER PRIMARY KEY, 
-                name TEXT, 
-                url TEXT, 
-                created_at DATETIME, 
-                updated_at DATETIME
-            );
-        """)
+if __name__ == "__main__":
+    create_user_table_sqlite()
