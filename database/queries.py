d59fad8e06433d0436ba20dd437160936d20c030
Jason Joannou <jjoannou@bscglobal.com>
2024-09-05 09:46:40 +0200

    Fixed query method

--- database/queries.py
+++ database/queries.py
@@ -1,25 +1,15 @@
 from .sqlite_connection import SQLiteConnection
 from .sql_connection import sql_connection
+from sqlalchemy import text
 
-sqlite_conn = SQLiteConnection(database="./test_db.db")
+sqlite_conn = SQLiteConnection(database="./database/test_db.db")
 # sql_conn = sql_connection()
 
 def check_if_number_exists_sqlite(from_number):
 
-    query = f"SELECT * FROM numbers WHERE number = :from_number"
+    query = f"SELECT * FROM USERS WHERE user_number = :from_number"
     with sqlite_conn.connect() as conn:
-        cursor = conn.execute(query, {'from_number': from_number})
-        result = cursor.fetchone()
-        if result:
-            return True
-        else:
-            return False
-        
-def check_if_number_exists_sql(from_number):
-
-    query = f"SELECT * FROM numbers WHERE number = :from_number"
-    with sql_conn.connect() as conn:
-        cursor = conn.execute(query, {'from_number': from_number})
+        cursor = conn.execute(text(query), {'from_number': from_number})
         result = cursor.fetchone()
         if result:
             return True
