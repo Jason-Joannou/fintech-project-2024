3918117dd7a4cccdd149385dc62a15fa1aaaf0b0
Jason Joannou <jjoannou@bscglobal.com>
2024-09-12 00:48:22 +0200

    added stokvel to route

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from routes import join_stokvel, onboarding, user_info, whatsapp_controller
+from routes import join_stokvel, onboarding, user_info, whatsapp_controller, stokvel_info
 
 app = Flask(__name__)
 
@@ -7,6 +7,7 @@ app.register_blueprint(join_stokvel)
 app.register_blueprint(onboarding)
 app.register_blueprint(user_info)
 app.register_blueprint(whatsapp_controller)
+app.register_blueprint(stokvel_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
