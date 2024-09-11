6d1e1ab245935601f41cb5822d9ad61bbf0551da
Jason Joannou <jjoannou@bscglobal.com>
2024-09-12 00:22:30 +0200

    initializing blueprints

--- api/app.py
+++ api/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from routes import join_stokvel, onboarding, user_info, whatsapp_controller
 
 app = Flask(__name__)
 
+app.register_blueprint(join_stokvel)
+app.register_blueprint(onboarding)
+app.register_blueprint(user_info)
+app.register_blueprint(whatsapp_controller)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ api/routes/user_info.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, request, jsonify
+
+user_info_bp = Blueprint("user_info", __name__)
+
+base_route = "/user_info"
+
+
+@user_info_bp.route(base_route)
+def user_info():
+    return "User Info API. This API endpoint is used for getting user information"
+
+
+@user_info_bp.route(f"{base_route}/all_users", methods=["POST"])
+def get_all_users():
+    return "Get all users API. This API endpoint is used for getting all users"
