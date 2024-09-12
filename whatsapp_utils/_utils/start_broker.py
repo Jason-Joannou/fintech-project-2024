21d6fbd9f3f4eff2f9974ffa14bee804da12f7f9
Jason Joannou <jjoannou@bscglobal.com>
2024-09-12 09:00:04 +0200

    Update to pylint config

--- whatsapp_utils/_utils/start_broker.py
+++
@@ -1,7 +0,0 @@
-from messageBroker.src.message_broker import MessageBroker
-
-from .action_handlers import process_action
-
-if __name__ == "__main__":
-    broker = MessageBroker()
-    broker.consume("action_queue", process_action)
