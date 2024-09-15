a92717eda09448aad1f2d91d50e40296cc52bef5
maryam.sonnie <maryam.sonnie@zutari.com>
2024-09-15 17:15:54 +0200

    added styled templates for user onboarding (incl a successful onboarding template. Created required routes, next to do: add in endpoint logic

--- api/app.py
+++ api/app.py
@@ -5,14 +5,14 @@ from api.routes.join_stokvel import join_stokvel_bp
 from api.routes.onboarding import onboarding_bp
 from api.routes.stokvel_info import stokvel_info_bp
 from api.routes.user_info import user_info_bp
-from api.routes.whatsapp_controller import whatsapp_bp
+# from api.routes.whatsapp_controller import whatsapp_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(join_stokvel_bp)
 app.register_blueprint(onboarding_bp)
 app.register_blueprint(user_info_bp)
-app.register_blueprint(whatsapp_bp)
+# app.register_blueprint(whatsapp_bp)
 app.register_blueprint(stokvel_info_bp)
 app.register_blueprint(example_template_bp)
 
