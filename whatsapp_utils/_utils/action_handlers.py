3c0466604ef7abd53cabf80f279e1b6e2b5ec087
Jason Joannou <jjoannou@bscglobal.com>
2024-09-16 10:57:41 +0200

    Action handlers

--- whatsapp_utils/_utils/action_handlers.py
+++ whatsapp_utils/_utils/action_handlers.py
@@ -6,10 +6,16 @@ def handle_action(from_number, action):
     docstring
     """
     if action == "1":
-        print("Here")
-        send_conversational_message("Please register through our online portal: https://stokvels.com")
+        msg = send_conversational_message("Please register through our online portal: https://stokvels.com")
         # Need to get a list of all available stockvels
     elif action == "2":
         pass
     else:
-        pass
+        msg = send_conversational_message(
+                "Sorry, I don't understand. Please activate the service by sending 'Hi' or 'Hello'"
+            )
+        print(
+            "Invalid message, please activate the service by sending 'Hi' or 'Hello'"
+        )
+
+    return msg
