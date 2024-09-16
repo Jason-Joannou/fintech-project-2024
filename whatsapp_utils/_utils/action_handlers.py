45d4c9bdfe7aaa93d1e5d517f2527e4877b0aa26
Jason Joannou <jjoannou@bscglobal.com>
2024-09-16 10:40:52 +0200

    Need to fix action handler

--- whatsapp_utils/_utils/action_handlers.py
+++ whatsapp_utils/_utils/action_handlers.py
@@ -1,17 +1,13 @@
-from .state_manager import StateManager
-from .twilio_client import TwilioClient
-
-twilio_client = TwilioClient()
-state_manager = StateManager()
+from .twilio_messenger import send_conversational_message
 
 
 def handle_action(from_number, action):
     """
     docstring
     """
-    state = state_manager.get_state(from_number)
-    if action == "1" and not state:
-        state_manager.set_state(from_number, {"action": "join_stokvel", "step": 1})
+    if action == "1":
+        print("Here")
+        send_conversational_message("Please register through our online portal: https://stokvels.com")
         # Need to get a list of all available stockvels
     elif action == "2":
         pass
