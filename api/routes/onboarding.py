213f82e061667fd9c39abfd295d5f8386ffa2cab
Jason Joannou <jjoannou@bscglobal.com>
2024-09-17 23:59:35 +0200

    Updated API route for onboarding to make use of the schema

--- api/routes/onboarding.py
+++ api/routes/onboarding.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, Response, redirect, render_template, request, url_for
 from sqlalchemy.exc import SQLAlchemyError
 
+from api.schemas.onboarding import OnboardUserSchema
 from database.queries import insert_user, insert_wallet
-from utils.user import User
-from utils.wallet import Wallet
 from whatsapp_utils._utils.twilio_messenger import send_notification_message
 
 onboarding_bp = Blueprint("onboarding", __name__)
@@ -25,44 +24,34 @@ def onboard_user() -> Response:
     Handles onboarding of a new user.
     """
     try:
-        name = request.form["name"]
-        surname = request.form["surname"]
-        cell_number = request.form["cellphone_number"]
-        id_number = request.form["id_number"]
-
-        user = User(
-            name=name,
-            surname=surname,
-            cell_number=cell_number,
-            id_number=id_number,
-            wallet_id="",
-        )
-        wallet = Wallet(user.id_number, user_wallet="ILP_test_string", user_balance=100)
+        user_data = OnboardUserSchema(
+            **request.form.to_dict()
+        )  # ** unpacks the dictionary
 
         insert_user(
-            user_id=user.id_number,
-            user_number=user.cell_number,
-            user_surname=user.surname,
-            user_name=user.name,
-            ilp_wallet=wallet.id,
+            user_id=user_data.id_number,
+            user_number=user_data.cellphone_number,
+            user_surname=user_data.surname,
+            user_name=user_data.name,
+            ilp_wallet=user_data.wallet_id,
         )
         insert_wallet(
-            user_id=user.id_number,
-            user_wallet=wallet.id,
-            userbalance=wallet.user_balance,
+            user_id=user_data.id_number,
+            user_wallet=user_data.wallet_id,
+            user_balance=100,
         )
         # Prepare the notification message
         notification_message = (
-            f"Welcome {user.name} {user.surname}!\n\n"
-            f"Your user ID: {user.id_number}\n"
-            f"Your wallet ID: {wallet.id}\n"
-            f"Your wallet balance: {wallet.user_balance}\n\n"
+            f"Welcome {user_data.name} {user_data.surname}!\n\n"
+            f"Your user ID: {user_data.id_number}\n"
+            f"Your wallet ID: {user_data.wallet_id}\n"
+            f"Your wallet balance: {100}\n\n"
             f"Thank you for registering with us!"
         )
 
         # Send the notification message
         send_notification_message(
-            to=f"whatsapp:{user.cell_number}", body=notification_message
+            to=f"whatsapp:{user_data.cellphone_number}", body=notification_message
         )
         return redirect(url_for("onboarding.success_user_creation"))
 
