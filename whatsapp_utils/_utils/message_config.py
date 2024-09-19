ce79dab236344f305f030be272133abe6aaa1984
Jason Joannou <jjoannou@bscglobal.com>
2024-09-20 00:06:34 +0200

    Updated message configs to states

--- whatsapp_utils/_utils/message_config.py
+++ whatsapp_utils/_utils/message_config.py
@@ -1,37 +1,61 @@
-REGISTER_NUMBER = {
-    "message": "Please provide your phone number in the format `+1XXXXXXXXXX`:"
-}
-
-JOIN_STOCKVEL = {
-    "step_1": {
-        "message": "Please select the stockvel you want to join:",
-    },
-    "step_2": {
-        "message": "Please provide the amount you want to contribute towards the stockvel:",
-    },
-}
-
-GREET_MESSAGE_REGISTERED = {
-    "message": """Hi there, welcome back! Please select one of the following options to proceed: 
-    1. Join a stockvel 
-    2. Leave a stockvel
-    3. Contribute to a stockvel
-    4. Payout from a stockvel
-    5. More Services"""
-}
-
-GREET_MESSAGE_UNREGISTERED = {
-    "message": """Hi there! We noticed this number is not registered with us. Registering your number is needed to start contributing to Stokvels.
-    Please select one of the following options to proceed: 
+UNREGISTERED_NUMBER = {
+    "tag": "unregistered_number",
+    "message": """
+    Hi there! We noticed this number is not registered with us. Registering your number is needed to start contributing to Stokvels.
+    Please select one of the following options to proceed:
     1. Register using Rafiki money
-    2. Register using MTN Mobile Money"""
+    2. Register using MTN Mobile Money
+    """,
+    "valid_actions": ["1", "2"],
+    "action_responses": {
+        "1": "Please register through our online portal: https://stokvels.com/register",
+        "2": "Please register through our online portal: https://stokvels.com/register",
+    },
 }
 
-MORE_SERVICES = {
+REGISTERED_NUMBER = {
+    "tag": "registered_number",
+    "message": """
+    Hi there, welcome back! Please select one of the following options to proceed:
+    1. Stokvel Services
+    2. Comunity Services
+    3. Financial Reports
+    """,
+    "valid_actions": ["1", "2", "3"],
+    "state": 0,
+}
+
+REGISTERED_NUMBER_ADMIN = {
+    "tag": "registered_number_admin",
+    "message": """
+    Hi there, welcome back! Please select one of the following options to proceed:
+    1. Stokvel Services
+    2. Community Services
+    3. Financial Reports
+    4. Admin Services
+    """,
+    "valid_actions": ["1", "2", "3", "4"],
+    "state": 0,
+}
+
+STOKVEL_SERVICES = {
+    "tag": "stokvel_services",
     "message": """
     Please select one of the following options to proceed:
-    1. View all available stockvels
-    2. Pay Reciepient"""
+    1. Create a stokvel
+    2. Join a stokvel
+    3. Contribute to a stockvel
+    4. Leave a stockvel
+    """,
+    "valid_actions": ["1", "2", "3", "4"],
+    "action_responses": {
+        "1": "Please register through our online portal: https://stokvels.com/register",
+        "2": "Please register through our online portal: https://stokvels.com/register",
+        "3": "Please specify the amount you want to contribute in Rands and the name of the stokvel you would like to contribute to.",
+        "4": "Please specify the stokvel you would like to leave.",
+    },
+    "state": 1,
 }
+
 
 # 25291716
