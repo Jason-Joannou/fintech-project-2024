1240b8bfd347b887c9b5394e471522ceeb12f9c2
Stefan0899 <stefanstapelberg8@gmail.com>
2024-09-20 09:47:41 +0200

    template changes

--- api/routes/example_template.py
+++ api/routes/example_template.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, Response, redirect, render_template, request, url_for
-
+from database.sqlite_connection import SQLiteConnection
+from sqlalchemy import text
 example_template_bp = Blueprint("example_template", __name__)
 
 
 BASE_ROUTE = "/example_template"
-
+db_conn = SQLiteConnection(database= "./database/test_db.db")
 
 @example_template_bp.route(BASE_ROUTE)
 def example_template() -> str:
@@ -23,6 +24,12 @@ def submit() -> Response:
     name = request.form["name"]
     email = request.form["email"]
     message = request.form["message"]
+
+    with db_conn.connect() as conn: 
+        #query = f"INSERT INTO comments (name, email, comment) VALUES ('{name}', '{email}', '{message}');"
+        query= 'INSERT INTO comments (name, email, comment) VALUES (:name, :email, :message);'
+        conn.execute(text(query), parameters= {"name":name, "email":email, "message":message})
+        conn.commit()
 
     # Process the data as needed (e.g., save to database, send email, etc.)
     # For this example, just print the data
