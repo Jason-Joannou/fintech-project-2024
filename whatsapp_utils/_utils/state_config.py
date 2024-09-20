c3020cd852bc043bcec6742112a2341ac6c409a9
Jason Joannou <jjoannou@bscglobal.com>
2024-09-20 13:39:46 +0200

    Updated state config
    - point of this is to be modular

--- whatsapp_utils/_utils/state_config.py
+++ whatsapp_utils/_utils/state_config.py
@@ -1,8 +1,22 @@
-from whatsapp_utils._utils.message_config import REGISTERED_NUMBER
+from whatsapp_utils._utils.message_config import (
+    ADMIN_SERVICES,
+    COMMUNITY_SERVICES,
+    FINANCIAL_REPORTS,
+    REGISTERED_NUMBER,
+    REGISTERED_NUMBER_ADMIN,
+    STOKVEL_SERVICES,
+    UNREGISTERED_NUMBER,
+)
 
 # Need to add messages states as we go
 MESSAGE_STATES = {
     "base_state": ["Hi", "Hello"],
-    "unrecognized_state": "Sorry, I don't understand that action.",
-    "registered_number_state": REGISTERED_NUMBER,
+    "unrecognized_state": "Sorry, I don't understand that action. The following actions are allowed in this state:\n",
+    REGISTERED_NUMBER["tag"]: REGISTERED_NUMBER,
+    REGISTERED_NUMBER["tag"]: REGISTERED_NUMBER_ADMIN,
+    UNREGISTERED_NUMBER["tag"]: UNREGISTERED_NUMBER,
+    ADMIN_SERVICES["tag"]: ADMIN_SERVICES,
+    COMMUNITY_SERVICES["tag"]: COMMUNITY_SERVICES,
+    FINANCIAL_REPORTS["tag"]: FINANCIAL_REPORTS,
+    STOKVEL_SERVICES["tag"]: STOKVEL_SERVICES,
 }
