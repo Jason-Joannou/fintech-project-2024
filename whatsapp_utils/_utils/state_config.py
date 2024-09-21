b224bd1e4a49b312190db7758c925600ceb37734
Jason Joannou <jjoannou@bscglobal.com>
2024-09-22 01:23:56 +0200

    TODO

--- whatsapp_utils/_utils/state_config.py
+++ whatsapp_utils/_utils/state_config.py
@@ -9,6 +9,7 @@ from whatsapp_utils._utils.message_config import (
 )
 
 # Need to add messages states as we go
+# TODO: Add more states
 MESSAGE_STATES = {
     "base_state": ["Hi", "Hello", "hi", "hello"],
     "unrecognized_state": "Sorry, I don't understand that action. The following actions are allowed in this state:\n",
