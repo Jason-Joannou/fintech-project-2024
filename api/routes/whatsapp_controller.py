c9dfb694a24a2d560d3bbb2244b3d9e0ee97ee47
Jason Joannou <jjoannou@bscglobal.com>
2024-09-22 00:53:13 +0200

    Changed whatsapp controller to use state manager

--- api/routes/whatsapp_controller.py
+++ api/routes/whatsapp_controller.py
@@ -3,10 +3,7 @@ from flask import Blueprint, request
 from database.user_queries.queries import check_if_number_exists_sqlite
 from whatsapp_utils._utils.action_handlers import handle_action
 from whatsapp_utils._utils.cache import Cache
-from whatsapp_utils._utils.message_config import (
-    GREET_MESSAGE_REGISTERED,
-    GREET_MESSAGE_UNREGISTERED,
-)
+from whatsapp_utils._utils.state_manager import MessageStateManager
 from whatsapp_utils._utils.twilio_messenger import send_conversational_message
 
 cache = Cache()
@@ -21,29 +18,10 @@ def whatsapp() -> str:
     """
     docstring
     """
-    incoming_msg = request.values.get("Body", "").lower()
+    incoming_msg = request.values.get("Body", "")
     from_number = request.values.get("From", "")
 
-    user = cache.get(from_number)
-    if user is None:
-        user = check_if_number_exists_sqlite(from_number=from_number)
-        if user:
-            cache.set(from_number, user)
-
-    if user:
-        print("here registered")
-        if incoming_msg in ["hi", "hello"]:
-            msg = send_conversational_message(GREET_MESSAGE_REGISTERED["message"])
-        else:
-            msg = handle_action(from_number=from_number, action=incoming_msg)
-
-    else:
-        print("User not registered")
-        if incoming_msg in ["hi", "hello"]:
-            msg = send_conversational_message(
-                message=GREET_MESSAGE_UNREGISTERED["message"]
-            )
-        else:
-            msg = handle_action(from_number=from_number, action=incoming_msg)
+    state_manager = MessageStateManager(user_number=from_number)
+    msg = state_manager.processes_user_request(user_action=incoming_msg)
 
     return str(msg)
