40c5712f82f952673f121b097a6ddb6581753a15
Jason Joannou <jjoannou@bscglobal.com>
2024-09-22 23:29:29 +0200

    Unpacking into stateschema

--- whatsapp_utils/_utils/state_config.py
+++ whatsapp_utils/_utils/state_config.py
@@ -1,3 +1,5 @@
+from typing import Dict, List, Union
+
 from whatsapp_utils._utils.message_config import (
     ADMIN_SERVICES,
     COMMUNITY_SERVICES,
@@ -7,16 +9,17 @@ from whatsapp_utils._utils.message_config import (
     STOKVEL_SERVICES,
     UNREGISTERED_NUMBER,
 )
+from whatsapp_utils.schemas.state_schema import StateSchema
 
 # Need to add messages states as we go
-MESSAGE_STATES = {
+MESSAGE_STATES: Dict[str, Union[StateSchema, Dict, List, str]] = {
     "base_state": ["Hi", "Hello", "hi", "hello"],
     "unrecognized_state": "Sorry, I don't understand that action. The following actions are allowed in this state:\n",
-    REGISTERED_NUMBER["tag"]: REGISTERED_NUMBER,
-    REGISTERED_NUMBER_ADMIN["tag"]: REGISTERED_NUMBER_ADMIN,
-    UNREGISTERED_NUMBER["tag"]: UNREGISTERED_NUMBER,
-    ADMIN_SERVICES["tag"]: ADMIN_SERVICES,
-    COMMUNITY_SERVICES["tag"]: COMMUNITY_SERVICES,
-    FINANCIAL_REPORTS["tag"]: FINANCIAL_REPORTS,
-    STOKVEL_SERVICES["tag"]: STOKVEL_SERVICES,
+    REGISTERED_NUMBER["tag"]: StateSchema(**REGISTERED_NUMBER),
+    REGISTERED_NUMBER_ADMIN["tag"]: StateSchema(**REGISTERED_NUMBER_ADMIN),
+    UNREGISTERED_NUMBER["tag"]: StateSchema(**UNREGISTERED_NUMBER),
+    ADMIN_SERVICES["tag"]: StateSchema(**ADMIN_SERVICES),
+    COMMUNITY_SERVICES["tag"]: StateSchema(**COMMUNITY_SERVICES),
+    FINANCIAL_REPORTS["tag"]: StateSchema(**FINANCIAL_REPORTS),
+    STOKVEL_SERVICES["tag"]: StateSchema(**STOKVEL_SERVICES),
 }
