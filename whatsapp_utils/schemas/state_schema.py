9fca43e040ca5f9b843a78264a4951c2b196ee70
Jason Joannou <jjoannou@bscglobal.com>
2024-09-22 23:07:08 +0200

    Changes to state schema

--- whatsapp_utils/schemas/state_schema.py
+++ whatsapp_utils/schemas/state_schema.py
@@ -26,10 +26,12 @@ class StateSchema(BaseModel):
     valid_actions: list[str] = Field(..., example=["1", "2", "3"])
     state: int = Field(..., example=1)
     action_responses: Optional[Dict] = Field(
-        None,
-        example="Please register through our online portal: https://stokvels.com/register",
+        {},
+        example={
+            "1": "Please register through our online portal: https://stokvels.com/register"
+        },
     )
     action_requests: Optional[Dict] = Field(
-        None, examples={"1": "/stokvels/total_stokvels"}
+        {}, examples={"1": "/stokvels/total_stokvels"}
     )
-    state_selection: Optional[Dict] = Field(None, examples={"1": "community_services"})
+    state_selection: Optional[Dict] = Field({}, examples={"1": "community_services"})
