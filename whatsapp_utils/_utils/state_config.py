a0acdf32976c396066ad61ce660f7dc05494a9a1
Jason Joannou <jjoannou@bscglobal.com>
2024-09-22 23:13:46 +0200

    state config change

--- whatsapp_utils/_utils/state_config.py
+++ whatsapp_utils/_utils/state_config.py
@@ -1,4 +1,4 @@
-from typing import Dict, Union
+from typing import Dict, List, Union
 
 from whatsapp_utils._utils.message_config import (
     ADMIN_SERVICES,
@@ -12,7 +12,7 @@ from whatsapp_utils._utils.message_config import (
 from whatsapp_utils.schemas.state_schema import StateSchema
 
 # Need to add messages states as we go
-MESSAGE_STATES: Dict[str, Union[StateSchema, Dict]] = {
+MESSAGE_STATES: Dict[str, Union[StateSchema, Dict, List, str]] = {
     "base_state": ["Hi", "Hello", "hi", "hello"],
     "unrecognized_state": "Sorry, I don't understand that action. The following actions are allowed in this state:\n",
     REGISTERED_NUMBER["tag"]: REGISTERED_NUMBER,
