4bdb06d6408bc4502f9fe7beb950557ffcfc019c
maryam.sonnie <maryam.sonnie@zutari.com>
2024-09-22 22:30:12 +0200

    added schema for stokvel

--- api/routes/create_stokvel.py
+++ api/routes/create_stokvel.py
@@ -1,13 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, Response, redirect, render_template, request, url_for
+# from sqlalchemy.exc import SQLAlchemyError
+
+# from api.schemas.onboarding import OnboardUserSchema
+# from database.queries import insert_user, insert_wallet
 
 create_stokvel_bp = Blueprint("create_stokvel", __name__)
 
 BASE_ROUTE = "/create_stokvel"
 
 
-@create_stokvel_bp.route(BASE_ROUTE)
-def create_stokvel() -> str:
+@create_stokvel_bp.route(f"{BASE_ROUTE}/stokvels", methods=["POST"])
+def onboard_stokvel() -> Response:
+    """
+    docstring
+    """
+    return redirect(
+        url_for(f'{BASE_ROUTE.removeprefix("/")}.success_stokvel_creation')
+    )
+
+@create_stokvel_bp.route("/success_stokvel_creation")
+def success_stokvel_creation() -> str:
     """
     docstrings
     """
-    return "Join Stokvel API. This API endpoint is used for joining a stokvel"
+    return render_template("stokvel_success.html")
+
+@create_stokvel_bp.route("/failed_stokvel_creation")
+def failed_stokvel_creation() -> str:
+    """
+    docstring
+    """
+    return render_template("stokvel_failed.html")
--- api/schemas/onboarding.py
+++ api/schemas/onboarding.py
@@ -11,3 +11,18 @@ class OnboardUserSchema(BaseModel):
     cellphone_number: str = Field(..., example="1234567890")
     id_number: str = Field(..., example="ID123456")
     wallet_id: str = Field("TestWaller", example="WalletABC")
+
+class RegisterStokvelSchema(BaseModel):
+    """
+    docstring
+    """
+    stokvel_id: int = Field(..., example=123)
+    stokvel_name: str = Field(..., example="John's Soccer Stokvel") #unique constraint here
+    ILP_wallet: str = Field(..., example="/johns_stokvel")
+    MOMO_wallet: str = Field(..., example="MOMO10255")
+    total_members: int = Field(..., example=15)
+    min_contributing_amount: float = Field(..., example=100.50)
+    max_number_of_contributors: int = Field(..., example=20)
+    Total_contributions: float = Field(..., example=153800)
+    created_at: str = Field(..., example="2024-10-01 00:00:00")
+    updated_at: str = Field(..., example="2024-10-01 00:00:00")
