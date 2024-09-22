fe615d63c49ceb9b6f8df30220f037ae5cddb223
Jason Joannou <jjoannou@bscglobal.com>
2024-09-22 23:45:42 +0200

    Updated message config types

--- whatsapp_utils/_utils/message_config.py
+++ whatsapp_utils/_utils/message_config.py
@@ -1,6 +1,6 @@
-from whatsapp_utils.schemas.state_schema import StateSchema
+from typing import Dict, List, Union
 
-UNREGISTERED_NUMBER: StateSchema = {
+UNREGISTERED_NUMBER: Dict[str, Union[str, List, Dict]] = {
     "tag": "unregistered_number",
     "message": """
     Hi there! We noticed this number is not registered with us. Registering your number is needed to start contributing to Stokvels.
@@ -16,7 +16,7 @@ UNREGISTERED_NUMBER: StateSchema = {
     "state": -1,
 }
 
-REGISTERED_NUMBER: StateSchema = {
+REGISTERED_NUMBER: Dict[str, Union[str, List, Dict]] = {
     "tag": "registered_number",
     "message": """
     Hi there, welcome back! Please select one of the following options to proceed:
@@ -33,7 +33,7 @@ REGISTERED_NUMBER: StateSchema = {
     "state": 0,
 }
 
-REGISTERED_NUMBER_ADMIN: StateSchema = {
+REGISTERED_NUMBER_ADMIN: Dict[str, Union[str, List, Dict]] = {
     "tag": "registered_number_admin",
     "message": """
     Hi there, welcome back! Please select one of the following options to proceed:
@@ -52,7 +52,7 @@ REGISTERED_NUMBER_ADMIN: StateSchema = {
     "state": 0,
 }
 
-STOKVEL_SERVICES: StateSchema = {
+STOKVEL_SERVICES: Dict[str, Union[str, List, Dict]] = {
     "tag": "stokvel_services",
     "message": """
     Please select one of the following options to proceed:
@@ -73,7 +73,7 @@ STOKVEL_SERVICES: StateSchema = {
     "state": 1,
 }
 
-COMMUNITY_SERVICES: StateSchema = {
+COMMUNITY_SERVICES: Dict[str, Union[str, List, Dict]] = {
     "tag": "community_services",
     "message": """
     Welcome to our community services! Please select one of the following options to proceed:
@@ -96,7 +96,7 @@ COMMUNITY_SERVICES: StateSchema = {
     "state": 1,
 }
 
-FINANCIAL_REPORTS: StateSchema = {
+FINANCIAL_REPORTS: Dict[str, Union[str, List, Dict]] = {
     "tag": "financial_reports",
     "message": """
     Welcome to your finacial reports services! Please select one of the following options to proceed:
@@ -115,7 +115,7 @@ FINANCIAL_REPORTS: StateSchema = {
     "state": 1,
 }
 
-ADMIN_SERVICES: StateSchema = {
+ADMIN_SERVICES: Dict[str, Union[str, List, Dict]] = {
     "tag": "admin_services",
     "message": """
     Welcome to your admin page! Please select one of the following options to proceed:
