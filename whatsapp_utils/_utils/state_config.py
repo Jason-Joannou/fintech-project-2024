7e2e481b87f3e2777c709ea75a8cac48601dbcdb
Jason Joannou <jjoannou@bscglobal.com>
2024-09-23 00:00:30 +0200

    Message types

--- whatsapp_utils/_utils/state_config.py
+++ whatsapp_utils/_utils/state_config.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, List, Union
 
 from whatsapp_utils._utils.message_config import (
     ADMIN_SERVICES,
@@ -10,8 +10,10 @@ from whatsapp_utils._utils.message_config import (
     UNREGISTERED_NUMBER,
 )
 
+MessageType = Union[str, List[str], Dict[str, Union[str, List, Dict, int]]]
+
 # Need to add messages states as we go
-MESSAGE_STATES: Dict[str, Any] = {
+MESSAGE_STATES: Dict[str, MessageType] = {
     "base_state": ["Hi", "Hello", "hi", "hello"],
     "unrecognized_state": "Sorry, I don't understand that action. The following actions are allowed in this state:\n",
     REGISTERED_NUMBER["tag"]: REGISTERED_NUMBER,
