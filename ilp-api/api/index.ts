5ae29c9d4d938cb96a60cf43f5a2fc1446f4fb24
Jason Joannou <jjoannou@bscglobal.com>
2024-09-23 23:28:44 +0200

    ILP Api

---
+++ ilp-api/api/index.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { getEnviromentVariables } from "../enviroment";
+
+const getBackendUrl = () => {
+  const { backendUrl } = getEnviromentVariables();
+  const apiVersion = "v1";
+  const fullUrl = `${backendUrl}/${apiVersion}`;
+  return fullUrl;
+};
+
+const getIlpUrl = () => {
+  const { ilpUrl } = getEnviromentVariables();
+  const apiVersion = "v1";
+  const fullUrl = `${ilpUrl}/${apiVersion}`;
+  return fullUrl;
+};
+
+const createAxiosInstance = (baseURL: string) =>
+  axios.create({
+    baseURL: baseURL,
+  });
+
+export { getBackendUrl, getIlpUrl, createAxiosInstance };
---
+++ ilp-api/routes.ts
@@ -0,0 +1,8 @@
+interface apiRoutes {
+  createDebitOrder: string;
+  createPaymentRequest: string;
+}
+export const apiRoutes = {
+  createDebitOrder: "/create-debit-order",
+  createPaymentRequest: "/create-payment-request",
+};
