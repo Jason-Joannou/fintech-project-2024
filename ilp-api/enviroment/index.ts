4bf0467f9a05ff97523d25ec7696bb6657071e44
Jason Joannou <jjoannou@bscglobal.com>
2024-09-24 12:01:12 +0200

    Updated enfiroment function to load env variables

--- ilp-api/enviroment/index.ts
+++ ilp-api/enviroment/index.ts
@@ -1,12 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export interface IEnvVariables {
   backendUrl: string;
   ilpUrl: string;
+  walletAddressUrl: string;
+  keyID: string;
+  privateKeyID: string;
 }
 
 export const getEnviromentVariables = (): IEnvVariables => {
   const variables = {
     backendUrl: process.env.BACKEND_URL || "http://localhost:3000",
     ilpUrl: process.env.ILP_URL || "http://localhost:3001",
+    walletAddressUrl: process.env.WALLET_ADDRESS_URL || "",
+    keyID: process.env.KEY_ID || "",
+    privateKeyID: process.env.PRIVATE_KEY_ID || "",
   };
 
   return variables;
