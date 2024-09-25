0fdb4b21d992bd4050c1620be4c2262d59de7655
Jason Joannou <jjoannou@bscglobal.com>
2024-09-25 23:21:02 +0200

    Pylint

--- whatsapp_utils/_utils/state_manager.py
+++ whatsapp_utils/_utils/state_manager.py
@@ -273,7 +273,7 @@ class MessageStateManager:
         """
         return self.current_state.get("state_selection", {})
 
-    def get_state_tags(self) -> Optional[str]:
+    def get_state_tags(self) -> str:
         """
         Retrieves the current and previous state tags from the database.
 
@@ -299,7 +299,7 @@ class MessageStateManager:
         Updates the local current and previous state attributes by retrieving
         the state tags from the database.
         """
-        self.current_state_tag: Optional[str] = self.get_state_tags()
+        self.current_state_tag = self.get_state_tags()
         self.current_state = (
             cast(StateSchema, MESSAGE_STATES[self.current_state_tag])
             if self.current_state_tag is not None
