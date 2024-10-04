84116101010d8595f2f141e2caec3f80bbe88c42
Jason Joannou <jjoannou@bscglobal.com>
2024-10-04 12:56:40 +0200

    Update to message config using actual api endpoints

--- whatsapp_utils/_utils/message_config.py
+++ whatsapp_utils/_utils/message_config.py
@@ -8,7 +8,7 @@ UNREGISTERED_NUMBER = {
     """,
     "valid_actions": ["1", "2"],
     "action_responses": {
-        "1": "Please register through our online portal: https://stokvels.com/register",
+        "1": "Please register through our online portal: http://stokveldigital.uksouth.azurecontainer.io/onboard",
         "2": "Please register through our online portal: https://stokvels.com/register",
     },
     "state": -1,
@@ -40,8 +40,8 @@ STOKVEL_SERVICES = {
     """,
     "valid_actions": ["1", "2", "3", "4"],
     "action_responses": {
-        "1": "Please join a stokvel through our online portal: https://stokvels.com/register",
-        "2": "Please create a new stokvel through our online portal: https://stokvels.com/register",
+        "1": "Please join a stokvel through our online portal: http://stokveldigital.uksouth.azurecontainer.io/stokvel/join_stokvel",
+        "2": "Please create a new stokvel through our online portal: http://stokveldigital.uksouth.azurecontainer.io/stokvel/create_stokvel",
     },
     "action_requests": {"3": "/stokvel/my_stokvels"},
     "state_selection": {"4": "back_state"},
@@ -157,7 +157,7 @@ Welcome to stokvel administration! Please select one of the following options to
     },
     "state_selection": {"6": "back_state"},
     "action_responses": {
-        "5": "View pending applications here: https://stokvels.com/register",
+        "5": "View pending applications here: http://stokveldigital.uksouth.azurecontainer.io/stokvel/approvals",
     },
     "input_request_states": {
         "1": {
