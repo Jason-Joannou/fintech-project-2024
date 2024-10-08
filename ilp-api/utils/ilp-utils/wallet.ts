86a12a019cfc4b6cdcc6d5dad3a18c6d0d784824
Jason Joannou <jjoannou@bscglobal.com>
2024-10-08 23:36:15 +0200

    Updated wallet address function

--- ilp-api/utils/ilp-utils/wallet.ts
+++ ilp-api/utils/ilp-utils/wallet.ts
@@ -1,4 +1,5 @@
 import { client } from "./client";
+import { OpenPaymentsClientError } from "@interledger/open-payments";
 
 export interface IWalletAddressResponse {
   id: string;
@@ -9,24 +10,37 @@ export interface IWalletAddressResponse {
   resourceServer: string;
 }
 
+export interface InvalidWalletResponse extends OpenPaymentsClientError {
+  status: number;
+  description: string;
+}
+
 const validateWalletAddress = async (
   walletAddress: string
-): Promise<IWalletAddressResponse | null> => {
+): Promise<IWalletAddressResponse | InvalidWalletResponse | null> => {
   try {
     const response = await client.walletAddress.get({
       url: walletAddress,
     });
 
     return response as IWalletAddressResponse; // This should be the wallet address information
-  } catch (error) {
-    console.log("Wallet Address Validation Failed: ", error);
+  } catch (error: unknown) {
+    if (error instanceof OpenPaymentsClientError) {
+      return {
+        ...error,
+        status: error.status || 500,
+        description: error.description || "Unknown error occurred",
+      } as InvalidWalletResponse;
+    }
+
+    console.error("Unexpected error type:", error);
     return null;
   }
 };
 
 export { validateWalletAddress };
 
-// const result = await validateWalletAddress(
-//   "https://ilp.rafiki.money/test_accounts"
-// );
-// console.log(result);
+const result = await validateWalletAddress(
+  "https://ilp.rafiki.money/test_account"
+);
+console.log(result);
