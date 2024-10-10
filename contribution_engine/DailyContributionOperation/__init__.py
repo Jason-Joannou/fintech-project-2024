d2e8f594edcaef7261051f076dd8f2ea90ed7207
Jason Joannou <jjoannou@bscglobal.com>
2024-10-11 00:13:01 +0200

    contribution azure function

---
+++ contribution_engine/DailyContributionOperation/__init__.py
@@ -0,0 +1,9 @@
+import logging
+from datetime import datetime, timezone
+
+from azure.functions import TimerRequest
+
+
+def main(DailyContributionOperation: TimerRequest) -> None:
+    # Logic for azure function
+    pass
