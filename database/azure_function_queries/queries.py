acfde8ce0fcb42e8f68713480d3687417ac13788
Jason Joannou <jjoannou@bscglobal.com>
2024-10-12 00:08:05 +0200

    Added write

--- database/azure_function_queries/queries.py
+++ database/azure_function_queries/queries.py
@@ -8,7 +8,7 @@ from database.sqlite_connection import SQLiteConnection
 db_conn = SQLiteConnection(database="./database/test_db.db")
 
 
-def dynamic_query(query: str, params: Dict) -> List[Dict]:
+def dynamic_read_operation(query: str, params: Dict) -> List[Dict]:
     """
     Executes a dynamic SQL query with provided parameters.
 
@@ -29,3 +29,28 @@ def dynamic_query(query: str, params: Dict) -> List[Dict]:
     except SQLAlchemyError as e:
         print(f"An error occurred during database query execution: {e}")
         return []
+
+
+def dynamic_write_operation(query: str, params: Dict) -> None:
+    """
+    Executes a dynamic SQL write operation with the provided parameters.
+
+    This function executes a SQL query for operations like INSERT, UPDATE, or DELETE.
+    It does not return any data.
+
+    Args:
+        query (str): The SQL query to execute. This should be a write operation such as
+                     an INSERT, UPDATE, or DELETE statement.
+        params (Dict): A dictionary of parameters to bind to the query, where the keys
+                       correspond to parameter names in the SQL query.
+
+    Raises:
+        SQLAlchemyError: If an error occurs during the execution of the query, the
+                         exception is caught and re-raised after logging the error.
+    """
+    try:
+        with db_conn.connect() as conn:
+            conn.execute(text(query), params)
+    except SQLAlchemyError as e:
+        print(f"An error occurred during database query execution: {e}")
+        raise e
