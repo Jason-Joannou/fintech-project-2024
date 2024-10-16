026da6b8292f7a2a3cdd50d56ae74a23e6d83470
Jason Joannou <jjoannou@bscglobal.com>
2024-10-17 00:57:58 +0200

    Payouts working

--- payout_engine/DailyPayoutOperation/__init__.py
+++ payout_engine/DailyPayoutOperation/__init__.py
@@ -44,6 +44,7 @@ def main(DailyPayoutOperation: TimerRequest) -> None:
             },
             timeout=10,
         )
+        print(payout_trigger_date_response.json())
 
         # Raise an exception for HTTP error responses
         payout_trigger_date_response.raise_for_status()
@@ -54,6 +55,7 @@ def main(DailyPayoutOperation: TimerRequest) -> None:
         logging.info(f"payout triggers: {payout_trigger_date}")
 
         if not payout_trigger_date:
+            print(payout_trigger_date)
             logging.info("No payout triggers found. Exiting.")
             return
 
@@ -140,7 +142,9 @@ def main(DailyPayoutOperation: TimerRequest) -> None:
                     and isinstance(deposits_data, list)
                     and len(deposits_data) > 0
                 ):
+                    print(deposits_data)
                     total_deposits = deposits_data[0].get("total_deposits", 0)
+                    total_deposits = total_deposits if total_deposits else 0
                     print(f"Total deposits to payout: {total_deposits}")
                 else:
                     print("No deposits found or error in the query response.")
@@ -366,177 +370,147 @@ def main(DailyPayoutOperation: TimerRequest) -> None:
 
                 print(f"Inserting transaction with parameters: {(parameters)}")
 
-                # Step 5: Update STOKVEL_MEMBERS if user_quote_id is not None
-                if stokvel_quote_id is not None:
-                    # Update user_quote_id to NULL
-                    update_response = requests.post(
-                        BASE_WRITE_ROUTE,
-                        json={
-                            "query": (
-                                """
-                                UPDATE STOKVEL_MEMBERS
-                                SET user_quote_id = NULL
-                                WHERE user_id = :user_id
-                                """
-                            ),
-                            "parameters": {"user_id": user_id},
-                        },
-                        timeout=10,
-                    )
+                # ILP Payment function for recurring payments
+                current_next_date = input_date
+                query = """
+                SELECT STOKVEL_MEMBERS.*,
+                    USERS.ILP_Wallet,
+                    STOKVELS.payout_frequency_duration,
+                        STOKVELS.contribution_period
+                    FROM STOKVEL_MEMBERS
 
-                    # ILP Payment function for initial payment
+                    JOIN USERS ON STOKVEL_MEMBERS.user_id = USERS.user_id
+                    JOIN STOKVELS ON STOKVEL_MEMBERS.stokvel_id = STOKVELS.stokvel_id WHERE STOKVEL_MEMBERS.stokvel_id = :stokvel_id
+                    AND STOKVEL_MEMBERS.user_id = :user_id
+                """
+                parameters = {"stokvel_id": stokvel_id, "user_id": user_id}
 
-                    # Raise an exception for HTTP error responses
-                    update_response.raise_for_status()
+                # Send the POST request to the API
+                stokvel_members_details_response = requests.post(
+                    BASE_READ_ROUTE,  # Assuming you're using a different route for reads
+                    json={
+                        "query": query,
+                        "parameters": parameters,
+                    },
+                    timeout=10,
+                )
 
-                    logging.info(f"Updated user_quote_id for user_id: {user_id}")
+                # Check for HTTP errors
+                stokvel_members_details_response.raise_for_status()
 
+                # Parse the JSON response
+                stokvel_members_result = stokvel_members_details_response.json()
+
+                # If result is found, return it
+                stokvel_members_details = stokvel_members_result[
+                    0
+                ]  # Assuming it's the first record
+
+                payload = {
+                    "sender_wallet_address": "https://ilp.rafiki.money/masterstokveladdress",
+                    "receiving_wallet_address": stokvel_members_details["ILP_wallet"],
+                    "manageUrl": stokvel_members_details["stokvel_payment_URI"],
+                    "previousToken": stokvel_members_details["stokvel_payment_token"],
+                    "payout_value": str(int(amount * 100)),
+                }
+
+                print(f"Payload: {payload}")
+                print(f"Previous Token: {payload['previousToken']}")
+
+                recurring_payment_response = requests.post(
+                    node_server_recurring_payment_with_interest,
+                    json=payload,
+                    timeout=10,
+                )
+                print("RESPONSE: \n", recurring_payment_response.json())
+
+                recurring_payment_response.raise_for_status()  # raise error making payment with node server
+
+                new_token = recurring_payment_response.json()["token"]
+                new_uri = recurring_payment_response.json()["manageurl"]
+
+                # update_stokvel_token_uri(stokvel_id, user_id, new_token, new_uri)
+
+                update_token_url_query = """
+                    UPDATE STOKVEL_MEMBERS
+                    SET stokvel_payment_token = :new_token,
+                        stokvel_payment_URI = :new_uri,
+                        updated_at = CURRENT_TIMESTAMP
+                    WHERE stokvel_id = :stokvel_id and user_id = :user_id
+                """
+
+                update_token_url_parameters = {
+                    "new_token": new_token,
+                    "new_uri": new_uri,
+                    "stokvel_id": stokvel_id,
+                    "user_id": user_id,
+                }
+
+                # Send the POST request to the API
+                stokvel_members_token_url_update_response = requests.post(
+                    BASE_WRITE_ROUTE,  # Assuming you're using a different route for reads
+                    json={
+                        "query": update_token_url_query,
+                        "parameters": update_token_url_parameters,
+                    },
+                    timeout=10,
+                )
+                # Check for HTTP errors
+                stokvel_members_token_url_update_response.raise_for_status()
+
+                # update to the next payout date
+
+                if stokvel_members_details["payout_frequency_duration"] == "Days":
+                    period_delta = timedelta(days=1)  # Increment by 1 day
+                elif stokvel_members_details["payout_frequency_duration"] == "Week":
+                    period_delta = timedelta(weeks=1)  # Increment by 1 week
+                elif stokvel_members_details["payout_frequency_duration"] == "Months":
+                    period_delta = relativedelta(months=1)  # Increment by 1 month
+                elif stokvel_members_details["payout_frequency_duration"] == "Years":
+                    period_delta = relativedelta(years=1)  # Increment by 1 year
                 else:
+                    raise ValueError("Invalid payout period specified.")
 
-                    # ILP Payment function for recurring payments
-                    current_next_date = input_date
-                    query = """
-                    SELECT STOKVEL_MEMBERS.*,
-                      USERS.ILP_Wallet,
-                        STOKVELS.payout_frequency_duration,
-                          STOKVELS.contribution_period
-                        FROM STOKVEL_MEMBERS
+                current_next_date_dt = datetime.strptime(current_next_date, "%Y-%m-%d")
+                next_date = (current_next_date_dt + period_delta).strftime("%Y-%m-%d")
 
-                        JOIN USERS ON STOKVEL_MEMBERS.user_id = USERS.user_id
-                        JOIN STOKVELS ON STOKVEL_MEMBERS.stokvel_id = STOKVELS.stokvel_id WHERE STOKVEL_MEMBERS.stokvel_id = :stokvel_id
-                        AND STOKVEL_MEMBERS.user_id = :user_id
-                    """
-                    parameters = {"stokvel_id": stokvel_id, "user_id": user_id}
+                update_next_payout_query = """
+                    UPDATE PAYOUTS
+                    SET PreviousDate = :PreviousDate, NextDate = :NextDate
+                    WHERE stokvel_id = :stokvel_id
+                """
 
-                    # Send the POST request to the API
-                    stokvel_members_details_response = requests.post(
-                        BASE_READ_ROUTE,  # Assuming you're using a different route for reads
-                        json={
-                            "query": query,
-                            "parameters": parameters,
-                        },
-                        timeout=10,
-                    )
+                date_update_parameters = {
+                    "PreviousDate": current_next_date,  # Set the current NextDate as PreviousDate
+                    "NextDate": next_date,  # Set the new calculated NextDate
+                    "stokvel_id": stokvel_id,
+                }
 
-                    # Check for HTTP errors
-                    stokvel_members_details_response.raise_for_status()
+                # Send the POST request to the API
+                stokvel_payouts_update_response = requests.post(
+                    BASE_WRITE_ROUTE,  # Assuming you're using a different route for reads
+                    json={
+                        "query": update_next_payout_query,
+                        "parameters": date_update_parameters,
+                    },
+                    timeout=10,
+                )
+                # Check for HTTP errors
+                stokvel_payouts_update_response.raise_for_status()
 
-                    # Parse the JSON response
-                    stokvel_members_result = stokvel_members_details_response.json()
+                # Raise an exception for HTTP error responses
+                payout_trigger_date_response.raise_for_status()
 
-                    # If result is found, return it
-                    stokvel_members_details = stokvel_members_result[
-                        0
-                    ]  # Assuming it's the first record
+                # Parse the JSON response
+                payout_trigger_date = payout_trigger_date_response.json()
 
-                    payload = {
-                        "sender_wallet_address": "https://ilp.rafiki.money/masterstokveladdress",
-                        "receiving_wallet_address": stokvel_members_details[
-                            "ILP_wallet"
-                        ],
-                        "manageUrl": stokvel_members_details["stokvel_payment_URI"],
-                        "previousToken": stokvel_members_details[
-                            "stokvel_payment_token"
-                        ],
-                        "payout_value": amount,
-                    }
+                logging.info(f"payout triggers: {payout_trigger_date}")
 
-                    recurring_payment_response = requests.post(
-                        node_server_recurring_payment_with_interest, json=payload
-                    )
-                    print("RESPONSE: \n", recurring_payment_response.json())
+                if not payout_trigger_date:
+                    logging.info("No payout triggers found. Exiting.")
+                    return
 
-                    recurring_payment_response.raise_for_status()  # raise error making payment with node server
-
-                    new_token = recurring_payment_response.json()["token"]
-                    new_uri = recurring_payment_response.json()["manageurl"]
-
-                    # update_stokvel_token_uri(stokvel_id, user_id, new_token, new_uri)
-
-                    update_token_url_query = """
-                        UPDATE STOKVEL_MEMBERS
-                        SET stokvel_payment_token = :new_token,
-                            stokvel_payment_URI = :new_uri,
-                            updated_at = CURRENT_TIMESTAMP
-                        WHERE stokvel_id = :stokvel_id and user_id = :user_id
-                    """
-
-                    update_token_url_parameters = {
-                        "new_token": new_token,
-                        "new_uri": new_uri,
-                        "stokvel_id": stokvel_id,
-                        "user_id": user_id,
-                    }
-
-                    # Send the POST request to the API
-                    stokvel_members_token_url_update_response = requests.post(
-                        BASE_WRITE_ROUTE,  # Assuming you're using a different route for reads
-                        json={
-                            "query": update_token_url_query,
-                            "parameters": update_token_url_parameters,
-                        },
-                        timeout=10,
-                    )
-                    # Check for HTTP errors
-                    stokvel_members_token_url_update_response.raise_for_status()
-
-                    # update to the next payout date
-
-                    if stokvel_members_details["payout_frequency_duration"] == "Days":
-                        period_delta = timedelta(days=1)  # Increment by 1 day
-                    elif stokvel_members_details["payout_frequency_duration"] == "Week":
-                        period_delta = timedelta(weeks=1)  # Increment by 1 week
-                    elif (
-                        stokvel_members_details["payout_frequency_duration"] == "Months"
-                    ):
-                        period_delta = relativedelta(months=1)  # Increment by 1 month
-                    elif (
-                        stokvel_members_details["payout_frequency_duration"] == "Years"
-                    ):
-                        period_delta = relativedelta(years=1)  # Increment by 1 year
-                    else:
-                        raise ValueError("Invalid payout period specified.")
-
-                    # Calculate the next contribution date
-                    next_date = current_next_date + period_delta
-
-                    update_next_payout_query = """
-                        UPDATE PAYOUTS
-                        SET PreviousDate = :PreviousDate, NextDate = :NextDate
-                        WHERE stokvel_id = :stokvel_id
-                    """
-
-                    date_update_parameters = {
-                        "PreviousDate": current_next_date,  # Set the current NextDate as PreviousDate
-                        "NextDate": next_date,  # Set the new calculated NextDate
-                        "stokvel_id": stokvel_id,
-                    }
-
-                    # Send the POST request to the API
-                    stokvel_payouts_update_response = requests.post(
-                        BASE_WRITE_ROUTE,  # Assuming you're using a different route for reads
-                        json={
-                            "query": update_next_payout_query,
-                            "parameters": date_update_parameters,
-                        },
-                        timeout=10,
-                    )
-                    # Check for HTTP errors
-                    stokvel_payouts_update_response.raise_for_status()
-
-                    # Raise an exception for HTTP error responses
-                    payout_trigger_date_response.raise_for_status()
-
-                    # Parse the JSON response
-                    payout_trigger_date = payout_trigger_date_response.json()
-
-                    logging.info(f"payout triggers: {payout_trigger_date}")
-
-                    if not payout_trigger_date:
-                        logging.info("No payout triggers found. Exiting.")
-                        return
-
-                    logging.info("recurring payout process completed successfully.")
+                logging.info("recurring payout process completed successfully.")
         return
 
     except Exception as e:
